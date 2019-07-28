764d8ac9a4fcf590b6a67580c609bfb07a2df5e3
Guillaume Boulant <gboulant@gmail.com>
2019-07-28 20:20:18 +0200

    refactoring

--- src/todogo/conf/config.go
+++ src/todogo/conf/config.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"sort"
 	"todogo/core"
 )
 
@@ -24,9 +23,6 @@ const (
 	NotebookDirname = "notes"
 	// defaultContextName is the name (and relative path) of the default context
 	defaultContextName = "default"
-
-	// noIndex is used to specify an undefined ContextArray index
-	noIndex = -1
 )
 
 var (
@@ -42,100 +38,6 @@ var (
 // DefaultContextPath returns a default path for this context name
 func DefaultContextPath(name string) string {
 	return filepath.Join(cfgdirpath, name)
-}
-
-// =========================================================================
-// Implementation of the a context Context
-
-// Context defines a workspace for a todo list
-type Context struct {
-	DirPath string
-	Name    string
-}
-
-// String implements the stringable interface for a Context
-func (context Context) String() string {
-	return fmt.Sprintf("%-8s: %s", context.Name, context.DirPath)
-}
-
-// JournalPath returns the absolute path of the journal of this context
-func (context Context) JournalPath() string {
-	return filepath.Join(context.DirPath, JournalFilename)
-}
-
-// ArchivePath returns the absolute path of the archive of this context
-func (context Context) ArchivePath() string {
-	return filepath.Join(context.DirPath, ArchiveFilename)
-}
-
-// NotesPath returns the absolute path of the notes directory of this context
-func (context Context) NotesPath() string {
-	return filepath.Join(context.DirPath, NotebookDirname)
-}
-
-// =========================================================================
-// Implementation of the collection of contexts ContextArray
-
-// ContextArray defines a list of Context workspaces
-type ContextArray []Context
-
-// String implements the stringable interface for a ContextArray
-func (contexts ContextArray) String() string {
-	s := ""
-	for i := 0; i < len(contexts); i++ {
-		s += fmt.Sprintf("%s\n", contexts[i].String())
-	}
-	return s
-}
-
-func (contexts *ContextArray) Remove(index int) error {
-	if index < 0 || index >= len(*contexts) {
-		return fmt.Errorf("ERR: index %d is out of range of contexts", index)
-	}
-	(*contexts)[index] = (*contexts)[len(*contexts)-1]
-	*contexts = (*contexts)[:len(*contexts)-1]
-	return nil
-}
-
-func (contexts *ContextArray) Append(context Context) error {
-	if contexts.GetContext(context.Name) != nil {
-		return fmt.Errorf("ERR: a context with name %s already exists", context.Name)
-	}
-	*contexts = append(*contexts, context)
-	return nil
-}
-
-func (contexts *ContextArray) SortByName() {
-	byName := func(i int, j int) bool {
-		return (*contexts)[i].Name < (*contexts)[j].Name
-	}
-	sort.Slice(*contexts, byName)
-}
-
-type filterFunction func(context Context) bool
-
-func (contexts ContextArray) index(filter filterFunction) int {
-	for i := 0; i < len(contexts); i++ {
-		if filter(contexts[i]) {
-			return i
-		}
-	}
-	return noIndex
-}
-
-func (contexts ContextArray) IndexFromName(name string) int {
-	filter := func(context Context) bool {
-		return context.Name == name
-	}
-	return contexts.index(filter)
-}
-
-func (contexts *ContextArray) GetContext(name string) *Context {
-	idx := contexts.IndexFromName(name)
-	if idx == noIndex {
-		return nil
-	}
-	return &(*contexts)[idx]
 }
 
 // =========================================================================
